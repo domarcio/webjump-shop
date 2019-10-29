72789efbcc15bfaeffeecf2697f08154e62a124d
domarcio <marciovinicius55@gmail.com>
2019-10-29 07:40:27 +0000

    Create relatedcategory entity

--- src/Category/Entity/RelatedCategory.php
+++ src/Category/Entity/RelatedCategory.php
@@ -8,7 +8,57 @@ declare(strict_types=1);
 
 namespace Nogues\Category\Entity;
 
+/**
+ * RelatedCategory entity.
+ *
+ * @package Nogues\Category\Entity
+ * @author  Marcio Vinicius <marciovinicius55@gmail.com>
+ */
 class RelatedCategory
 {
+    /**
+     * Primary Key ID
+     *
+     * @var int
+     */
+    private $id;
 
+    /**
+     * Category ID.
+     *
+     * @var int
+     */
+    private $categoryId;
+
+    /**
+     * Related Entity ID.
+     *
+     * @var int
+     */
+    private $relatedId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryId(): ?int
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(int $categoryId): void
+    {
+        $this->categoryId = $categoryId;
+    }
+
+    public function getRelatedId(): ?int
+    {
+        return $this->relatedId;
+    }
+
+    public function setRelatedId(int $relatedId): void
+    {
+        $this->relatedId = $relatedId;
+    }
 }
